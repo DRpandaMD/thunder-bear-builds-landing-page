97ac8b7ec50ce3217aef1248ea4aa43b49a124bc
DRpandaMD <michael.zarate.ce@gmail.com>
2024-05-04 23:10:12 +0000

    added lighting and camera controls to scene

--- ThunderBearBuildsLandingPage/main.js
+++ ThunderBearBuildsLandingPage/main.js
@@ -4,10 +4,14 @@
 import '/style.css'
 import * as THREE from 'three'
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 // scene creates a scene
 const scene = new THREE.Scene();
+
 // using the most common type of camera where we have (FOV, AspectRatio, Fustrum nearplane, Fustrum Far Plane)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 // lets get a renderer, we are telling it to use the canvas we created referenced in the html and css files
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
@@ -17,7 +21,10 @@ renderer.setPixelRatio(window.devicePixelRatio);
 // make the canvas full screen by setting the renderer size to the size of the screen
 renderer.setSize(window.innerWidth, window.innerHeight);
 // now we set the position of the camera along the Z axis.  Remember this is in 3D!!
-camera.position.setZ(20);
+camera.position.setZ(38);
+
+// adding orbit controls to allow me to move around the scene
+const controls = new OrbitControls( camera, renderer.domElement);
 
 // now we can draw
 renderer.render(scene, camera);
@@ -26,10 +33,37 @@ renderer.render(scene, camera);
 // then give the object its material
 // then create the mesh object
 // then add it to the scene
-const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-const material = new THREE.MeshBasicMaterial({color: 0xf60404, wireframe: true } );
+const geometry = new THREE.TorusKnotGeometry(8, 2, 100, 32);
+const material = new THREE.MeshStandardMaterial( {color: 0xf60404} );
 const red_crazy_torus = new THREE.Mesh(geometry, material);
 scene.add(red_crazy_torus);
+
+//lighting
+// adding ambient light so I can see something
+const amb_light = new THREE.AmbientLight( 0x404040 );
+scene.add(amb_light);
+
+
+//point light 1, positioned up and to the left from the z axis
+const point_light01 = new THREE.PointLight (0xffffff, 5, 0, 0);
+point_light01.position.set( -10, 7, 5 );
+scene.add(point_light01);
+
+//point light 2, 
+const point_light02 = new THREE.PointLight(0xffffff, 5, 0, 0);
+point_light02.position.set(5, -5, 5);
+scene.add(point_light02);
+
+
+//point light helper to help me see where in the world the point light is
+const sphere_size = 1;
+const light_helper01 = new THREE.PointLightHelper(point_light01, sphere_size);
+const light_helper02 = new THREE.PointLightHelper(point_light02, sphere_size);
+scene.add(light_helper01, light_helper02);
+
+//grid helper
+const grid_helper = new THREE.GridHelper(100, 100);
+scene.add(grid_helper);
 
 //set up rendering automation
 function animate() {
@@ -38,6 +72,8 @@ function animate() {
   red_crazy_torus.rotation.x += 0.01;
   red_crazy_torus.rotation.y += 0.005;
   red_crazy_torus.rotation.z += 0.01;
+
+  controls.update();
   
   renderer.render( scene, camera );
 
